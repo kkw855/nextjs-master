5be39e04f15a248e287b39b4013955ce55dfdcf0
kkw855 <kkw855@gmail.com>
2024-07-17 22:45:47 +0900

    🔨 devConf: ESLint, Prettier, Husky, lint-staged

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: false,
+  tabWidth: 2,
+  singleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
+
+export default config
